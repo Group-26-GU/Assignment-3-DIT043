70a691b0b3814603e93219848f829e0ccf9034ea
cch0000 <guscampca@student.gu.se>
2022-10-10 14:38:55 +0200

    Finished 1.9

--- src/Company.java
+++ src/Company.java
@@ -9,14 +9,7 @@ public class Company {
         return "Employee " + id + " was registered successfully.";
     }
 
-    public void updateName(String id, String newName){
-        employees.get(id).setName(newName);
-    }
 
-    public void updateSalary(String id, Double newSalary){
-        employees.get(id).setSalaryGross(newSalary);
-    }
-    
                                                         // 1.4 remove an employee
     public String removeEmployee(String id){ 
         boolean containsKey= employees.containsKey(id);
@@ -49,12 +42,45 @@ public class Company {
         }
         return summ;
     }
-
     public  double getNetSumm() {
         double summ = 0;
         for(var key: employees.keySet()) {
             summ += employees.get(key).getSalaryNet();
         }
         return summ;
+    }
+    public String updatedSuccesfully(String id){
+        return "Employee " + id + " was updated successfully";
+    }
+    public String updateName(String id, String newName){
+        employees.get(id).setName(newName);
+        return updatedSuccesfully(id);
+    }
+
+    public String updateSalary(String id, Double newSalary){
+        employees.get(id).setSalaryGross(newSalary);
+        return updatedSuccesfully(id);
+    }
+
+    public String updateDegree(String id, String newDegree){
+        if (employees.get(id) instanceof Manager){
+            ((Manager) employees.get(id)).setDegree(newDegree);
+            return updatedSuccesfully(id);
+        }
+        else return "";
+    }
+    public String updateDepartment(String id, String newDeparment){
+        if (employees.get(id) instanceof Director){
+            ((Director) employees.get(id)).setDepartment(newDeparment);
+            return updatedSuccesfully(id);
+        }
+        else return "";
+    }
+    public String updateGpa( String id, int gpa){
+        if (employees.get(id) instanceof Intern){
+            ((Intern) employees.get(id)).setGpa(gpa);
+            return updatedSuccesfully(id);
+        }
+        else return "";
     }
 }
